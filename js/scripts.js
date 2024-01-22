4d083b1222fd25d7d63b6b37b318296c3a406d46
Alex-Sandoval <62357693+Alex-Sandoval@users.noreply.github.com>
2024-01-22 17:07:48 -0600

    mods a formatos

--- js/scripts.js
+++ js/scripts.js
@@ -212,11 +212,11 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Espere...</strong> se está guardando su informacion .'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'){
+           // && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Oops!</strong> Tu codigo de invitado es incorrecto'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbwBaFvy10ymIeF5U92iJ6RieoSLBMu1oMktgoK_U5zX3BPDHzpQLg6tU-EH8qQVBaImmA/exec', data)
                 .done(function (data) {
@@ -230,7 +230,7 @@ $(document).ready(function () {
                 })
                 .fail(function (data) {
                     console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Oops!</strong> hay un problema con el servidor. '));
                 });
         }
     });
